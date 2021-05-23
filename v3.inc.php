0975c87b59aca140fef9123baa34bc086d1d7b78
Daniel Marschall <info@daniel-marschall.de>
2021-05-23 21:11:56 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/php_antispam/trunk@3 e916ab1d-55f1-4b27-9e94-5aed3e6a70e5

--- v3.inc.php
+++ v3.inc.php
@@ -3,35 +3,35 @@
 // PHP-AntiSpam-Funktion "secure_email", Version 3.05 of 2018-07-20
 // von Daniel Marschall [www.daniel-marschall.de]
 
+if (!function_exists('alas_js_crypt'))
+{
+	function alas_js_crypt($text)
+	{
+		$tmp = '';
+		for ($i=0; $i<strlen($text); $i++)
+		{
+			$tmp .= 'document.write("&#'.ord(substr($text, $i, 1)).';");';
+		}
+		return $tmp;
+	}
+}
+
+if (!function_exists('alas_js_write'))
+{
+	function alas_js_write($text)
+	{
+		$text = str_replace('\\', '\\\\', $text);
+		$text = str_replace('"', '\"', $text);
+		$text = str_replace('/', '\/', $text); // W3C Validation </a> -> <\/a>
+		return 'document.write("'.$text.'");';
+	}
+}
+
 function secure_email($email, $linktext, $crypt_linktext)
 {
 	// No new lines to avoid a JavaScript error!
 	$linktext = str_replace("\r", ' ', $linktext);
 	$linktext = str_replace("\n", ' ', $linktext);
-
-	if (!function_exists('alas_js_crypt'))
-	{
-		function alas_js_crypt($text)
-		{
-			$tmp = '';
-			for ($i=0; $i<strlen($text); $i++)
-			{
-				$tmp .= 'document.write("&#'.ord(substr($text, $i, 1)).';");';
-			}
-			return $tmp;
-		}
-	}
-
-	if (!function_exists('alas_js_write'))
-	{
-		function alas_js_write($text)
-		{
-			$text = str_replace('\\', '\\\\', $text);
-			$text = str_replace('"', '\"', $text);
-			$text = str_replace('/', '\/', $text); // W3C Validation </a> -> <\/a>
-			return 'document.write("'.$text.'");';
-		}
-	}
 
 	$aus = '';
 	if ($email != '')
